c4deb99329516d966f141dd56075550468227181
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2022-11-14 16:53:15 +0100

    get request for specific product with their id from the params with its category

--- routers/productRouter.js
+++ routers/productRouter.js
@@ -3,13 +3,25 @@ const Products = require("../models").products;
 const { Router } = express;
 const router = new Router();
 //GET - /products: Returns a list of products with their categories
-router.get("/", async (request, response, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const products = await Products.findAll();
-    response.send(products);
+    res.send(products);
   } catch (e) {
     console.log(e.message);
     next(e);
+  }
+});
+
+//GET - /products/:id Returns a specific product with it's category
+
+router.get("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  const product = await Products.findByPk(id);
+  if (!product) {
+    res.status(404).send("product not found");
+  } else {
+    res.status(200).send(product);
   }
 });
 
